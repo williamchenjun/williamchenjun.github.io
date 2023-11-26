f731b23fb43300ca654df4d70e7c74ea8b788f44
William Chen <79821802+williamchenjun@users.noreply.github.com>
2023-11-26 04:56:26 +0100

    fixed layout issues

--- header.js
+++ header.js
@@ -1,10 +1,13 @@
-$(document).ready(() => {
+$(window).on('load', () => {
     const links = [...document.querySelectorAll('ul#navbar-links > li > a')];
-    const linksWidths = links.map(el => el.offsetWidth);
+    const linksWidths = links.map(el => el.getBoundingClientRect().width);
     const gap = 20;
     const indicatorHalfWidth = 3.5;
 
-    $('div.page-indicator').css('margin-left', linksWidths.slice(0, links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))).reduce((tot, curr) => tot + curr, 0) + document.querySelector('ul#navbar-links > li > a.active-page').offsetWidth / 2 + links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))*gap - indicatorHalfWidth + 'px');
+    $('ul#navbar-links > li > a').ready(()=>{
+        const init_pos = linksWidths.slice(0, links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))).reduce((tot, curr) => tot + curr, 0) + document.querySelector('ul#navbar-links > li > a.active-page').getBoundingClientRect().width / 2 + links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))*gap - indicatorHalfWidth;
+        $('div.page-indicator').css('margin-left', init_pos);
+    });
     
     $('ul#navbar-links > li > a').on('mouseover', (event) => {
         
@@ -17,7 +20,11 @@ $(document).ready(() => {
     });
 
     $('ul#navbar-links').on('mouseleave', (event) => {
-        let offset = linksWidths.slice(0, links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))).reduce((tot, curr) => tot + curr, 0) + document.querySelector('ul#navbar-links > li > a.active-page').offsetWidth / 2 + links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))*gap - indicatorHalfWidth;
+        let offset = linksWidths.slice(0, links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))).reduce((tot, curr) => tot + curr, 0) + document.querySelector('ul#navbar-links > li > a.active-page').getBoundingClientRect().width / 2 + links.findIndex(x => x == document.querySelector('ul#navbar-links > li > a.active-page'))*gap - indicatorHalfWidth;
         gsap.to('div.page-indicator', {marginLeft: offset, duration: .4, ease: 'elastic.out(1,0.7)'});
     });
+
+    // setTimeout(()=> {
+        
+    // }, 3000);
 });
